679e47ffea6a803a2d8bbbea26c6b9e8f0983cd0
heo <foresty110@gmail.com>
2025-12-02 12:24:43 +0900

    feat: url 확인 필터 구현

--- src/main/java/org/example/realtimechat/security/SecurityConfig.java
+++ src/main/java/org/example/realtimechat/security/SecurityConfig.java
@@ -1,6 +1,5 @@
 package org.example.realtimechat.security;
 
-import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -18,7 +17,7 @@ public class SecurityConfig {
         http.csrf(csrf -> csrf.disable())//쿠키 세션의 로그인 방식은 사용하지 않도록 설정
                 .formLogin(formLogin -> formLogin.disable())//기본 제공 폼 로그인 사용 제거
                 .httpBasic(httpBasicAuth -> httpBasicAuth.disable()) //옛날 인증 방식 제외
-                .addFilterBefore(new UrlLoggingFilter(), UsernamePasswordAuthenticationFilter.class)
+                .addFilterBefore(new UrlCheckFilter(), UsernamePasswordAuthenticationFilter.class)
                 .authorizeHttpRequests(auth->
                         auth.requestMatchers("/api/auth/**").permitAll()
                                 .anyRequest().authenticated()
